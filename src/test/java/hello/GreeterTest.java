478774f6d574ddda1cdcd035f7381640a00d544c
Bondad <jayson.j.b.bondad@accenture.com>
2021-10-13 10:34:34 +0800

    Minor edit

--- src/test/java/hello/GreeterTest.java
+++ src/test/java/hello/GreeterTest.java
@@ -10,8 +10,27 @@ public class GreeterTest {
   private Greeter greeter = new Greeter();
 
   @Test
+  //SuccessTest
   public void greeterSaysHello() {
+    assertThat(greeter.sayHello(), containsString("Hello"));
+  }
+  @Test
+  public void greeterSaysHello1() {
+    assertThat(greeter.sayHello(), containsString("World"));
+  }
+  @Test
+  public void greeterSaysHello2() {
     assertThat(greeter.sayHello(), containsString("Bye"));
   }
-
+  
+  //FailedTest
+  @Test
+  public void greeterSaysHello3() {
+    assertThat(greeter.sayHello(), containsString("Fail"));
+  }
+  @Test
+  public void greeterSaysHello4() {
+    assertThat(greeter.sayHello(), containsString("Fail"));
+  }
+  
 }
